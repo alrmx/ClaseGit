29d1c71056ba9103989b4bb12f57fd96575216a9
alrmx <redes-alr@outlook.pt>
2022-08-09 23:14:26 +0000

    update main.py

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 
 app = FastAPI()
 
@@ -9,3 +10,7 @@ async def root():
 @app.get("/myname/{name}")
 async def myName(name: str):
     return {"message": f"Hello {name} this is my new API!"}
+
+@app.get("/albuquerque")
+async def redirect_typer():
+    return RedirectResponse("https://images.pexels.com/photos/13125805/pexels-photo-13125805.jpeg")
