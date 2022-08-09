86d3b458b498d17ca17f8db0fcd572a69a18db29
alrmx <redes-alr@outlook.pt>
2022-08-09 17:30:39 +0000

    Se modifico el archivo requirements

--- app/main.py
+++ app/main.py
@@ -2,11 +2,10 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello World this is my new API!"}
 
-@app.get("/myname/(name)")
+@app.get("/myname/{name}")
 async def myName(name: str):
-    return {"message": "Hello (name) this is my new API"}
+    return {"message": f"Hello {name} this is my new API!"}
